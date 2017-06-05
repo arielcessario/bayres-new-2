dd86e6271af7b940f87c8b836c6fc1cf44477a26
Ariel Cessario <arielcessario@gmail.com>
2017-06-05 14:04:53 -0300

    Update MyDBi.php

--- app/includes/MyDBi.php
+++ app/includes/MyDBi.php
@@ -118,8 +118,7 @@ class MysqliDb
      * @param string $db
      * @param int $port
      */
-    public function __construct($host ='192.185.4.175', $username = 'arielces_test', $password = '*DJ[c(n@)[ku', $db = 'arielces_bayres_test', $port = NULL)
-    //public function __construct($host = 'localhost', $username = 'root', $password = '', $db = 'arielces_bayres', $port = NULL)
+  public function __construct($host = 'localhost', $username = 'root', $password = '', $db = 'arielces_bayres', $port = NULL)
     {
         $this->host = $host;
         $this->username = $username;
